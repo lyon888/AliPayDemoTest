654b5cf06a96c8dbcde4b3f2116232906b151e79
lyon <lyon_lzy@sina.cn>
2016-08-19 10:22:18 +0800

    初始化

---
+++ AliPayDemoTest/AliPayDemoTest/AppDelegate.m
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.m
+//  AliPayDemoTest
+//
+//  Created by 众网合一 on 16/6/14.
+//  Copyright © 2016年 GdZwhy. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <AlipaySDK/AlipaySDK.h>
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES;
+}
+
+// NOTE: 9.0以后使用新API接口
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+    NSLog(@"url = %@",url);
+    
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
---
+++ AliPayDemoTest/AliPayDemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliPayDemoTest
+//
+//  Created by 众网合一 on 16/6/14.
+//  Copyright © 2016年 GdZwhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
